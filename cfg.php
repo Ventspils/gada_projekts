ef6edafe7a4bc11a1614207154253ae4f2345f00
Ventspils <vents.babris@gmail.com>
2025-12-03 14:54:38 +0200

    forgor this one

--- cfg.php
+++ cfg.php
@@ -3,15 +3,17 @@ session_start();
 $code = $_POST['pass'];
 $code = SHA1($code);
 include('db.php');
-$result = $mysqli->query("SELECT * FROM vb_users WHERE password=$code");
+$result = $mysqli->query("SELECT * FROM vb_users WHERE code='$code'");
 if (mysqli_num_rows($result) > 0){
     while ($row = $result ->fetch_assoc()){
        $_SESSION["id"] = $row['id_users'];
     }
     header("Location: members.php");
+    exit;
 }
 else{
     header("Location: index.php");
+    exit;
 }
 
 ?>
