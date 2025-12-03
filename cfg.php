0e650bd2af97aba7e6c62eb44f97b8357dda8f27
Ventspils <vents.babris@gmail.com>
2025-12-03 14:14:52 +0200

    not neede stuff
    like the title says

--- cfg.php
+++ cfg.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
-$user = $_POST['login'];
-$parole = $_POST['pass'];
-$parole = SHA1($parole);
+$code = $_POST['pass'];
+$code = SHA1($code);
 include('db.php');
-$result = $mysqli->query("SELECT * FROM vb_users WHERE username='$user' AND password='$parole'");
+$result = $mysqli->query("SELECT * FROM vb_users WHERE password='$parole'");
 if (mysqli_num_rows($result) > 0){
     while ($row = $result ->fetch_assoc()){
        $_SESSION["id"] = $row['id_users'];
--- index.php
+++ index.php
@@ -22,71 +22,8 @@
     </div>
         <br>
     <button type="submit" class="btn btn-primary">Submit</button>
-    <span class="switch-text" id="showRegister">Reģistrēties</span>
     </form>
 </div>
-<div class='register'>
-    <form action='register_cfg.php' method='post'>
-        <div class="mb-3">
-            <input name='vards' type="text" class="form-control" placeholder="Name" required>
-        </div>
-        <br>
-        <div class="mb-3">
-            <input name='uzvards' type="text" class="form-control" placeholder="Surname" required>
-        </div>
-        <br>
-        <div class="mb-3">
-            <input name='email' type="email" class="form-control" placeholder="Email address" required>
-        </div>
-        <br>
-        <div class="mb-3">
-            <input name='password' type="password" class="form-control" placeholder="Password" required>
-        </div>
-        <br>
-        <div class="mb-3">
-            <input name='klase' type="text" class="form-control" placeholder="Klase (piem. 11.b)" required>
-        </div>
-        <br>
-        <button type="submit" class="btn btn-primary">Reģistrēties</button>
-        <span class="switch-text" id="showLogin">Pieteikties</span>
-    </form>
-</div>
-<script>
-// Formu pārslēgšana
-const loginBox = document.querySelector('.login');
-const registerBox = document.querySelector('.register');
-document.getElementById('showRegister').onclick = () => {
-    loginBox.style.display = 'none';
-    registerBox.style.display = 'block';
-};
-document.getElementById('showLogin').onclick = () => {
-    registerBox.style.display = 'none';
-    loginBox.style.display = 'block';
-};
 
-// AJAX reģistrācija
-document.getElementById('registerForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const formData = new FormData(this);
-
-    fetch('register_cfg.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(res => res.json())
-    .then(data => {
-        alert(data.message);
-        if (data.success) {
-            registerBox.style.display = 'none';
-            loginBox.style.display = 'block';
-        }
-    })
-    .catch(err => {
-        alert("Kļūda savienojumā ar serveri.");
-        console.error(err);
-    });
-});
-</script>
 </body>
 </html>
